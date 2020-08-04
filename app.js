e310f04ee2eaf39649c5ade399323dbf5a9b3cfd
Mak <makotorasahi@gmail.com>
2020-08-04 14:33:48 -0700

    nutrition list working

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ document.getElementById('search').addEventListener('click', event => {
 
     axios.get(`https://api.nal.usda.gov/fdc/v1/foods/search?api_key=ckuDCJZTvGODnGVYDC4FTRlLhXJhmjEtg3slZV4Z&query=${name}`)
         .then(res => {
-        console.log(res.data)
+            console.log('usda food data for nutrition')
+            console.log(res.data)
+            nutritionInfo(res.data)
 
     })
     .catch(err => { console.log(err) })
@@ -20,13 +22,29 @@ document.getElementById('search').addEventListener('click', event => {
 
     axios.get(`https://api.spoonacular.com/recipes/complexSearch?apiKey=85a06dbd80b548e1822e70e6227765b4&query=${name}`)
         .then(res => {
-        console.log(res.data)
+            console.log('spoonacular for image')
+            console.log(res.data)
 
-        document.getElementById('nutrition').innerHTML = `
-            <img src = ${res.data.results[0].image} >
+            document.getElementById('nutrition').innerHTML = `
+                <img src = ${res.data.results[0].image} >
         
-        `
+            `
 
     })
     .catch(err => { console.log(err) })
   })
+
+
+// Write food nutrient data to page
+
+function nutritionInfo(searchedThingy) {
+    console.log(searchedThingy.foods[0].foodNutrients)
+    let nutritionList = searchedThingy.foods[0].foodNutrients
+    document.getElementById('nutritionList').innerHTML = ''
+    for (let i = 0; i < nutritionList.length; i++) {
+        let nutritionElem = document.createElement('li')
+        nutritionElem.innerHTML = `${nutritionList[i].nutrientName}: ${nutritionList[i].value}: ${nutritionList[i].unitName}`
+        document.getElementById('nutritionList').append(nutritionElem)
+    }
+}
+
