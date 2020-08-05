84f30d2ae08007b7737350a0ac91c433b7c24df9
Mak <makotorasahi@gmail.com>
2020-08-05 10:57:45 -0700

    more info from api search

--- app.js
+++ app.js
@@ -1,38 +1,38 @@
 // Search button is clicked. Run through API
-document.getElementById('search').addEventListener('click', event => {
-    event.preventDefault()
+// document.getElementById('search').addEventListener('click', event => {
+//     event.preventDefault()
 
-    let name = document.getElementById('name').value
+//     let name = document.getElementById('name').value
 
-    axios.get(`https://api.nal.usda.gov/fdc/v1/foods/search?api_key=ckuDCJZTvGODnGVYDC4FTRlLhXJhmjEtg3slZV4Z&query=${name}`)
-        .then(res => {
-            console.log('usda food data for nutrition')
-            console.log(res.data)
-            nutritionInfo(res.data)
+//     axios.get(`https://api.nal.usda.gov/fdc/v1/foods/search?api_key=ckuDCJZTvGODnGVYDC4FTRlLhXJhmjEtg3slZV4Z&query=${name}`)
+//         .then(res => {
+//             console.log('usda food data for nutrition')
+//             console.log(res.data)
+//             nutritionInfo(res.data)
 
-    })
-    .catch(err => { console.log(err) })
-  })
+//     })
+//     .catch(err => { console.log(err) })
+//   })
 
 
-  document.getElementById('search').addEventListener('click', event => {
-    event.preventDefault()
+//   document.getElementById('search').addEventListener('click', event => {
+//     event.preventDefault()
 
-    let name = document.getElementById('name').value
+//     let name = document.getElementById('name').value
 
-    axios.get(`https://api.spoonacular.com/recipes/complexSearch?apiKey=85a06dbd80b548e1822e70e6227765b4&query=${name}`)
-        .then(res => {
-            console.log('spoonacular for image')
-            console.log(res.data)
+//     axios.get(`https://api.spoonacular.com/recipes/complexSearch?apiKey=85a06dbd80b548e1822e70e6227765b4&query=${name}`)
+//         .then(res => {
+//             console.log('spoonacular for image')
+//             console.log(res.data)
 
-            document.getElementById('nutrition').innerHTML = `
-                <img src = ${res.data.results[0].image} >
+//             document.getElementById('nutrition').innerHTML = `
+//                 <img src = ${res.data.results[0].image} >
         
-            `
+//             `
 
-    })
-    .catch(err => { console.log(err) })
-  })
+//     })
+//     .catch(err => { console.log(err) })
+//   })
 
 
 // Write food nutrient data to page
@@ -47,3 +47,24 @@ function nutritionInfo(searchedThingy) {
         document.getElementById('nutritionList').append(nutritionElem)
     }
 }
+
+
+// Testing indepth food search
+document.getElementById('search').addEventListener('click', event => {
+    event.preventDefault()
+
+    let name = document.getElementById('name').value
+
+    axios.get(`https://api.spoonacular.com/recipes/random?apiKey=85a06dbd80b548e1822e70e6227765b4&query=${name}`)
+        .then(res => {
+            console.log('spoonacular by nutrients')
+            console.log(res.data)
+
+            document.getElementById('nutrition').innerHTML = `
+                <img src = ${res.data.results[0].image} >
+        
+            `
+
+    })
+    .catch(err => { console.log(err) })
+  })
